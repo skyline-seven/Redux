085769150ce972cdf6989aeaad3264cf1c849993
skyline-seven <92408170+skyline-seven@users.noreply.github.com>
2021-12-03 12:39:29 -0500

    Add files via upload

---
+++ Redux.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+from tkinter import ttk
+import os
+
+window = Tk()
+window.title("Redux")
+window.configure(width=600, height=600)
+
+# typebox
+data = Entry()
+data.pack()
+
+window.mainloop()
